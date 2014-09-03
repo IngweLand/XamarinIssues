80ac8e7ce0d447084313b17e2b57b7bcb6b042e8
IngweLand <ingweland@gmail.com>
2014-09-03 12:35:56 +0300

    CALayerDelegateTest init commit

---
+++ CALayerDelegateCrashTest/CALayerDelegateCrashTest/TestLayerDelegate.cs
@@ -0,0 +1,24 @@
+﻿using MonoTouch.CoreAnimation;
+using MonoTouch.CoreGraphics;
+using System;
+using System.Drawing;
+using MonoTouch.UIKit;
+
+namespace Test
+{
+    public class TestLayerDelegate : CALayerDelegate
+    {
+        public override void DrawLayer(CALayer layer, CGContext context)
+        {
+			context.SetFillColor(UIColor.Green.CGColor);
+			context.FillRect(layer.Frame);
+		}
+
+		protected override void Dispose (bool disposing)
+		{
+			Console.WriteLine ("---dispose {0}", this);
+			base.Dispose (disposing);
+			Console.WriteLine ("---postdispose {0}", this);
+		}      
+    }
+}
---
+++ CALayerDelegateCrashTest/CALayerDelegateCrashTest/TestViewController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using MonoTouch.CoreAnimation;
+
+namespace Test
+{
+	public partial class TestViewController : UIViewController
+	{
+		private UIView _testView;
+
+		public TestViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			_testView = new UIView (View.Frame);
+			View.Add (_testView);
+			//case 1
+			_testView.Layer.Delegate = new TestLayerDelegate ();
+			//case 2
+			//_testView.Layer.Delegate = new CALayerDelegate ();
+			//_testView.Layer.SetNeedsDisplay ();
+		}
+
+		protected override void Dispose (bool disposing)
+		{
+			Console.WriteLine ("---dispose {0}", this);
+			base.Dispose (disposing);
+			Console.WriteLine ("---postdispose {0}", this);
+		}
+
+	}
+}
+
